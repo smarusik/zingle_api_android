1ec0b33d516b1cea24b092926cd200432cf2b46f
smarusik <smarusik@gmail.com>
2015-08-22 17:02:47 +0300

    ZingleTimeZoneServices implemented.

---
+++ libzinglesdk/src/main/java/me/zingle/api/sdk/services/ZingleTimeZoneServices.java
@@ -0,0 +1,83 @@
+package me.zingle.api.sdk.services;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.zingle.api.sdk.Exceptions.UnsuccessfullRequestEx;
+import me.zingle.api.sdk.dao.ZingleConnection;
+import me.zingle.api.sdk.dao.ZingleQuery;
+import me.zingle.api.sdk.dto.ResponseDTO;
+import me.zingle.api.sdk.model.ZingleTimeZone;
+
+import static me.zingle.api.sdk.dao.RequestMethods.GET;
+
+/**
+ * Created by SLAVA 08 2015.
+ */
+public class ZingleTimeZoneServices {
+    static final String resoursePath="/time-zones";
+
+    private ServiceDelegate<List<ZingleTimeZone>> delegate;
+
+    public ZingleTimeZoneServices(ServiceDelegate<List<ZingleTimeZone>> delegate) {
+        this.delegate = delegate;
+    }
+
+    public void setDelegate(ServiceDelegate<List<ZingleTimeZone>> delegate) {
+        this.delegate = delegate;
+    }
+
+    static ZingleTimeZone mapper(JSONObject source) throws JSONException {
+        return new ZingleTimeZone(source.getString("display_name"));
+    }
+
+    static List<ZingleTimeZone> arrayMapper(JSONArray source) throws JSONException {
+        int i=0;
+        JSONObject temp=source.optJSONObject(i++);
+
+        List<ZingleTimeZone> retList=new ArrayList<>();
+
+        while(temp!=null){
+            retList.add(mapper(temp));
+            temp=source.optJSONObject(i++);
+        }
+
+        return retList;
+    }
+
+    public static List<ZingleTimeZone> list() throws UnsuccessfullRequestEx {
+
+        ZingleQuery query = new ZingleQuery(GET, resoursePath);
+
+        ResponseDTO response = ZingleConnection.getInstance().send(query);
+
+        if(response.getResponseCode()==200){
+            JSONArray result=response.getData().getJSONArray("result");
+            return arrayMapper(result);
+        }
+        else
+            throw new UnsuccessfullRequestEx("Error list()",response.getResponseCode(),response.getResponseStr());
+    }
+
+    public boolean listAsync(){
+
+        Thread th=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    delegate.processResult(list());
+                }catch (UnsuccessfullRequestEx e){
+                    delegate.processError(e.getResponceCode(),e.getResponceStr());
+                }
+            }
+        });
+
+        th.start();
+
+        return true;
+    }
+}
