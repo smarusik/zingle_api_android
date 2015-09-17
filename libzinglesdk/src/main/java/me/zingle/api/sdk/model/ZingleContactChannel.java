919cd0d1fdcc299a460eef86b914c4a0481d9705
smarusik <smarusik@gmail.com>
2015-09-17 10:37:17 +0300

    Service and Contact Channel added.

---
+++ libzinglesdk/src/main/java/me/zingle/api/sdk/model/ZingleContactChannel.java
@@ -0,0 +1,73 @@
+package me.zingle.api.sdk.model;
+
+/**
+ * Created by SLAVA 09 2015.
+ */
+public class ZingleContactChannel {
+    private Integer id;
+    private ZingleChannelType type;
+    private String displayName;
+    private String value;
+    private Boolean is_default;
+    private Boolean is_default_for_type;
+
+    public ZingleContactChannel() {
+    }
+
+    public ZingleContactChannel(Integer id, ZingleChannelType type, String displayName, String value, Boolean is_default, Boolean is_default_for_type) {
+        this.id = id;
+        this.type = type;
+        this.displayName = displayName;
+        this.value = value;
+        this.is_default = is_default;
+        this.is_default_for_type = is_default_for_type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ZingleChannelType getType() {
+        return type;
+    }
+
+    public void setType(ZingleChannelType type) {
+        this.type = type;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Boolean getIs_default() {
+        return is_default;
+    }
+
+    public void setIs_default(Boolean is_default) {
+        this.is_default = is_default;
+    }
+
+    public Boolean getIs_default_for_type() {
+        return is_default_for_type;
+    }
+
+    public void setIs_default_for_type(Boolean is_default_for_type) {
+        this.is_default_for_type = is_default_for_type;
+    }
+}
---
+++ libzinglesdk/src/main/java/me/zingle/api/sdk/model/ZingleServiceChannel.java
@@ -0,0 +1,109 @@
+package me.zingle.api.sdk.model;
+
+import java.net.URL;
+
+/**
+ * Created by SLAVA 09 2015.
+ */
+public class ZingleServiceChannel {
+    private Integer id;
+    private ZingleChannelType type;
+    private Boolean default_for_type;
+    private String value;
+    private URL inbound_notification_url=null;
+    private String inbound_notification_request_type=null;
+    private String inbound_notification_data_format=null;
+    private URL outbound_notification_url=null;
+    private String outbound_notification_request_type=null;
+    private String outbound_notification_data_format=null;
+
+    public ZingleServiceChannel() {
+    }
+
+    public ZingleServiceChannel(Integer id, ZingleChannelType type, Boolean default_for_type, String value) {
+        this.id = id;
+        this.type = type;
+        this.default_for_type = default_for_type;
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ZingleChannelType getType() {
+        return type;
+    }
+
+    public void setType(ZingleChannelType type) {
+        this.type = type;
+    }
+
+    public Boolean getDefault_for_type() {
+        return default_for_type;
+    }
+
+    public void setDefault_for_type(Boolean default_for_type) {
+        this.default_for_type = default_for_type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public URL getInbound_notification_url() {
+        return inbound_notification_url;
+    }
+
+    public void setInbound_notification_url(URL inbound_notification_url) {
+        this.inbound_notification_url = inbound_notification_url;
+    }
+
+    public String getInbound_notification_request_type() {
+        return inbound_notification_request_type;
+    }
+
+    public void setInbound_notification_request_type(String inbound_notification_request_type) {
+        this.inbound_notification_request_type = inbound_notification_request_type;
+    }
+
+    public String getInbound_notification_data_format() {
+        return inbound_notification_data_format;
+    }
+
+    public void setInbound_notification_data_format(String inbound_notification_data_format) {
+        this.inbound_notification_data_format = inbound_notification_data_format;
+    }
+
+    public URL getOutbound_notification_url() {
+        return outbound_notification_url;
+    }
+
+    public void setOutbound_notification_url(URL outbound_notification_url) {
+        this.outbound_notification_url = outbound_notification_url;
+    }
+
+    public String getOutbound_notification_request_type() {
+        return outbound_notification_request_type;
+    }
+
+    public void setOutbound_notification_request_type(String outbound_notification_request_type) {
+        this.outbound_notification_request_type = outbound_notification_request_type;
+    }
+
+    public String getOutbound_notification_data_format() {
+        return outbound_notification_data_format;
+    }
+
+    public void setOutbound_notification_data_format(String outbound_notification_data_format) {
+        this.outbound_notification_data_format = outbound_notification_data_format;
+    }
+}
