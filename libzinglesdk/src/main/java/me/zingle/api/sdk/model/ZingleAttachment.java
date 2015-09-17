cfe58adf0b826270ec3ffcd6c86a1284bf152730
smarusik <smarusik@gmail.com>
2015-09-17 18:17:49 +0300

    Zingle Attachment got constructors and getters-setters.

--- libzinglesdk/src/main/java/me/zingle/api/sdk/model/ZingleAttachment.java
+++ libzinglesdk/src/main/java/me/zingle/api/sdk/model/ZingleAttachment.java
@@ -9,4 +9,41 @@ public class ZingleAttachment {
     private String mimeType;
     private URL url;
     private byte[] data; //base64 encoded data
+
+    public ZingleAttachment() {
+    }
+
+    public ZingleAttachment(byte[] data, String mimeType) {
+        this.data = data;
+        this.mimeType = mimeType;
+    }
+
+    public ZingleAttachment(String mimeType, URL url) {
+        this.mimeType = mimeType;
+        this.url = url;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
 }
